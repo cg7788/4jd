020af05081050e9a5d79b5aea3617ddb7e94b83b
cg7788 <43008813+cg7788@users.noreply.github.com>
2023-07-27 21:51:11 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '669d790b-67b4-48d2-ba15-b29fb40fa63c';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
